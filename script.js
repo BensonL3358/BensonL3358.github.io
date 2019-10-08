36f1009120131a75709a2bd1fa96eafa3f781716
BensonL3358 <56265228+BensonL3358@users.noreply.github.com>
2019-10-08 11:07:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Benson L."
+let userName = "BensonL3358";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
